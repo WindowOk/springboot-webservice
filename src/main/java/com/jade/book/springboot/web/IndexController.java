912a45df49187a9d4562a9f58786b5691c0d2455
WindowOk <107999260+WindowOk@users.noreply.github.com>
2022-08-07 16:11:16 +0900

    commit

--- src/main/java/com/jade/book/springboot/web/IndexController.java
+++ src/main/java/com/jade/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jade.book.springboot.web;
 
+import com.jade.book.springboot.config.auth.LoginUser;
+import com.jade.book.springboot.config.auth.dto.SessionUser;
 import com.jade.book.springboot.service.posts.PostsService;
 import com.jade.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //어노테이션 기반으로 개선
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            // userName -> userNames로 변경 : 구글 계정 이름이 아닌 OS 사용자로 인식하여 충돌 방지를 위해
+            model.addAttribute("userNames", user.getName());
+        }
         return "index";
     }
 
